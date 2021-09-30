e8914f718cd3adc3528f9ffbfbea850b79c25a3e
ReyFey <vic.betsch@gmail.com>
2021-09-30 12:35:06 +0200

    :white_check_mark: Add UnitTest classes (5 tests)

--- TestCompteBancaire/UnitTest1.cs
+++ TestCompteBancaire/UnitTest1.cs
@@ -1,3 +1,4 @@
+using CompteBancaire;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestCompteBancaire
@@ -6,8 +7,33 @@ namespace TestCompteBancaire
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_soldeCreationCompteB()
         {
+            CompteB CompteTest = new CompteB();
+            Assert.AreEqual(0, CompteTest.getSolde());
+        }
+        
+        [TestMethod]
+        public void Test_soldeCreationCompteB500()
+        {
+            CompteB CompteTest = new CompteB(500);
+            Assert.AreEqual(500, CompteTest.getSolde());
+        }
+        
+        [TestMethod]
+        public void Test_soldeApresDebit()
+        {
+            CompteB CompteTest = new CompteB(500);
+            CompteTest.Debiter(100);
+            Assert.AreEqual(400, CompteTest.getSolde());
+        }
+        
+        [TestMethod]
+        public void Test_SoldeApresCredit()
+        {
+            CompteB CompteTest = new CompteB(500);
+            CompteTest.Crediter(100);
+            Assert.AreEqual(600, CompteTest.getSolde());
         }
     }
 }
---
+++ TestCompteBancaire/UnitTest2.cs
@@ -0,0 +1,19 @@
+﻿using CompteBancaire;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCompteBancaire
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void Test_GetNbCompte()
+        {
+            CompteB Compte1 = new CompteB();
+            CompteB Compte2 = new CompteB();
+            CompteB Compte3 = new CompteB();
+            CompteE Compte4 = new CompteE();
+            Assert.AreEqual(4, Compte1.getNbComptes());
+        }
+    }
+}
